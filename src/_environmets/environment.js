4c7a5e931d227e25bdafef997988e22cd773917b
Jordi <jordirosado@jordirosado.com>
2023-01-13 12:29:10 +0100

    para git pages

--- src/_environmets/environment.js
+++ src/_environmets/environment.js
@@ -2,6 +2,7 @@ export const environment = {
    production: false,
 
    // api
-   BASE_API_URL: "http://localhost:3000",
+   // BASE_API_URL: "http://localhost:3000",
+   BASE_API_URL: "https://mongomoviesdb-production.up.railway.app",
    IMAGES_URL: "https://image.tmdb.org/t/p",
 };
